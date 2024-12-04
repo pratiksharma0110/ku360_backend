14b466a41c87c435945a84e9d602a48b792503f7
pratiksharma0110 <pratiksharma0110@gmail.com>
2024-12-04 13:38:35 +0545

    queries related to user

---
+++ queries/userQueries.js
@@ -0,0 +1,15 @@
+const instertIntoProfile =
+  "INSERT INTO UserProfiles (user_id, department, year, semester) VALUES ($1, $2, $3, $4)";
+
+const getUserDetails = `
+      SELECT 
+        u.firstname || ' ' || u.lastname AS full_name,
+        u.email,
+        up.department,
+        up.year,
+        up.semester
+      FROM users u
+      JOIN userprofiles up ON u.user_id = up.user_id
+      WHERE u.user_id = $1;
+    `;
+module.exports = { instertIntoProfile, getUserDetails };
