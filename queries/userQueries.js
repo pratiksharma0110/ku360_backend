48fbb05c08f5d06888dbc03d44986ab564f86f83
pratiksharma0110 <pratiksharma0110@gmail.com>
2024-12-25 13:57:23 +0545

    progile by id fetchd for existing profile

--- queries/userQueries.js
+++ queries/userQueries.js
@@ -1,6 +1,9 @@
 const instertIntoProfile =
-  "INSERT INTO UserProfiles (user_id, school,department, batch) VALUES ($1, $2, $3, $4)";
+  "INSERT INTO userprofiles (user_id, school,department, year,semester) VALUES ($1, $2, $3, $4,$5)";
 
+const getProfileByUserId = `
+  SELECT * FROM userprofiles WHERE user_id = $1;
+`;
 const getUserDetails = `
       SELECT 
         u.firstname || ' ' || u.lastname AS full_name,
@@ -12,4 +15,4 @@ const getUserDetails = `
       JOIN userprofiles up ON u.user_id = up.user_id
       WHERE u.user_id = $1;
     `;
-module.exports = { instertIntoProfile, getUserDetails };
+module.exports = { instertIntoProfile, getUserDetails, getProfileByUserId };
