1f9d0dd86b5692549872612b336ce9baa1d0e14b
pratiksharma0110 <pratiksharma0110@gmail.com>
2024-12-25 13:57:31 +0545

    try catch block

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,26 +2,31 @@ const pool = require("../database/postgres");
 const userQueries = require("../queries/userQueries");
 
 const userDetails = async (req, res) => {
-  const userId = req.user;
-  if (!userId) {
-    return res.status(400).json({
-      message: "Please log in to continue",
-    });
-  }
+  try {
+    const userId = req.user;
+    if (!userId) {
+      return res.status(400).json({
+        message: "Please log in to continue",
+      });
+    }
 
-  const userInformation = await pool.query(userQueries.getUserDetails, [
-    userId,
-  ]);
-  if (!userInformation.rows.length) {
-    return res.status(404).json({
-      message: "User not found",
-    });
-  }
+    const userInformation = await pool.query(userQueries.getUserDetails, [
+      userId,
+    ]);
+    if (!userInformation.rows.length) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
 
-  res.status(200).json(userInformation.rows[0]);
+    res.status(200).json(userInformation.rows[0]);
+  } catch (err) {
+    console.log(`Internal Server Error: ${err.message}`);
+    res.sendStatus(500);
+  }
 };
 
-const editUserProfile = async (req, res) => {};
+const editUserProfile = async (req, res) => { };
 
 module.exports = {
   userDetails,
