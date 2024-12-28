98c7aa965dad22f0c2326ee62bda677c5f18545a
Alaxini <lamichhaneprabal2159@gmail.com>
2024-12-28 11:29:38 +0545

    added otp

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
-require("dotenv").config();
-const express = require("express");
-const cookieParser = require("cookie-parser");
+import dotenv from 'dotenv';
+import express, { json, urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import authRoutes from './routes/auth/authRoutes.js';
+import onBoardingRoutes from './routes/onBoardingRoute.js';
+import userRoutes from './routes/userRoutes.js';
+import noticeRoute from './routes/noticeRoutes.js';
+import otpRoutes from './routes/otp.js'; 
 
-const authRoutes = require("./routes/auth/authRoutes");
-const onBoardingRoutes = require("./routes/onBoardingRoute");
-const userRoutes = require("./routes/userRoutes");
-const noticeRoute = require("./routes/noticeRoutes");
+// Load environment variables
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-//middlewares:
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.JWT_SECRET));
-//routes
-app.use("/auth", authRoutes);
-app.use("/onBoarding", onBoardingRoutes);
-app.use("/getCurrentUser", userRoutes);
-app.use("/getExamNotice", noticeRoute);
+// Middlewares
+app.use(json()); // Built-in middleware to parse JSON requests
+app.use(urlencoded({ extended: false })); // Built-in middleware to parse URL-encoded data
+app.use(cookieParser(process.env.JWT_SECRET)); // Parse cookies
+app.use(cors()); // Enable CORS for all routes
 
-//default route
-app.get("/", (req, res) => {
-  res.send("backend server");
+// Routes
+app.use('/api', otpRoutes); // All OTP endpoints will be prefixed with /api
+app.use('/auth', authRoutes);
+app.use('/onBoarding', onBoardingRoutes);
+app.use('/getCurrentUser', userRoutes);
+app.use('/getExamNotice', noticeRoute);
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('Backend server is running.');
 });
-//start server
+
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server Running on ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
