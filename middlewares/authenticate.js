ff94fb7e02c3b781b91a2bc7bedae72cdfb123bd
Pratik Sharma <pratiksharma0110@gmail.com>
2025-01-08 09:46:33 +0545

    used authorization header for verification instead of cookies

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -2,11 +2,12 @@ const jwt = require("jsonwebtoken");
 
 const authenticateUser = async (req, res, next) => {
   try {
-    const token = req.cookies.token;
-    if (!token)
+    const authHeader = req.headers["authorization"];
+    if (!authHeader)
       return res.status(401).json({
         message: "Access denied",
       });
+    const token = authHeader.split(" ")[1];
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
