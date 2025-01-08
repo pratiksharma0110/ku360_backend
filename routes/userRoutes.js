99751288cdf9db2988d0ab84bc4baca7f0686ad9
Pratik Sharma <pratiksharma0110@gmail.com>
2025-01-08 09:47:00 +0545

    uploadMiddeware for upload route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,11 @@ const {
   editUserProfile,
 } = require("../controllers/userController");
 const upload = require("../controllers/uploadController");
+const uploadMiddleware = require("../middlewares/uploadMiddleware");
 const router = express.Router();
 
 //routes;
 router.get("/getCurrentUser", authenticateUser, userDetails);
 router.post("/editProfile", authenticateUser, editUserProfile);
-router.post("/upload", upload);
+router.post("/upload", uploadMiddleware, upload);
 module.exports = router;
