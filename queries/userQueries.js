9d162ca7d0fd7c1e481d45d6b3c93f666716a925
Pratik Sharma <pratiksharma0110@gmail.com>
2025-01-11 15:40:06 +0545

    queries changed as db chnages

--- queries/userQueries.js
+++ queries/userQueries.js
@@ -1,5 +1,5 @@
 const instertIntoProfile =
-  "INSERT INTO userprofiles (user_id, school,department, year_id,semester_id,profile_image) VALUES ($1, $2, $3, $4,$5,$6)";
+  "INSERT INTO userprofiles (user_id, school,department_id, year_id,semester_id,profile_image,has_onboarded) VALUES ($1, $2, $3, $4,$5,$6,$7)";
 
 const getProfileByUserId = `
   SELECT * FROM userprofiles WHERE user_id = $1;
@@ -10,13 +10,15 @@ const getUserDetails = `
     u.email,
     up.school,
     up.profile_image,
-    up.department,
+    up.department_id,
+    d.name AS department,
     y.name AS year_name,     
     s.name AS semester_name 
   FROM users u
   JOIN userprofiles up ON u.user_id = up.user_id
   JOIN years y ON up.year_id = y.id           
-  JOIN semesters s ON up.semester_id = s.id  
+  JOIN semesters s ON up.semester_id = s.id 
+  JOIN departments d ON up.department_id = d.id
   WHERE u.user_id = $1;
 `;
 
@@ -36,7 +38,7 @@ SELECT
 FROM 
   courses c
 JOIN 
-  course_semesters cs ON c.id = cs.course_id
+  course_semesters cs ON c.sub_code = cs.sub_code
 JOIN 
   years y ON cs.year_id = y.id
 JOIN 
@@ -46,11 +48,18 @@ WHERE
   AND s.id = $2;
 `;
 const fetchRoutine = `
-Select r.time,c.sub_code,c.sub_name
-from routines r
-JOIN courses c on r.course_id = c.id
-WHERE day = $1
-
+SELECT 
+  r.sub_code,
+  c.sub_name,
+  COALESCE(t.title || ' ', '') || t.firstname || ' ' || t.lastname AS instructor_name,
+  r.time 
+FROM routines r
+JOIN teachers t ON r.teacher_id = t.teacher_id
+JOIN courses c ON r.sub_code = c.sub_code
+WHERE r.years_id = $1
+  AND r.semester_id = $2 
+  AND r.day = $3
+  AND r.department_id = $4;
 `;
 
 module.exports = {
